a880d0e819835a5d68d75883dc2a13260302ce16
DreamChen-1996 <376367714@qq,com>
2024-09-15 17:21:24 +0800

    commit src file

---
+++ src/test.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+//20240824:CSAPP
+//³ÂÀíÏë
+typedef unsigned char* byte_pointer;
+
+void show_bytes(byte_pointer start,size_t len)
+{
+	size_t i;
+	for()
+	{
+
+	}
+
+}
+
+int main()
+{
+	unsigned int aaa = 5624;
+	int bbb = 0xf000;
+	printf("%d\n",bbb*bbb);
+	return 0;
+}
